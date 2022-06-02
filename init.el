27c0905ee232db8b8b6c91c8794ec07b9bdb626c
Dmytro Prylipko <prylipko@safead.de>
2022-06-02 12:04:12 +0000

    Updated config

--- init.el
+++ init.el
@@ -1,31 +1,46 @@
 (require 'package)
 
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
 (package-initialize)
+; (package-refresh-contents)
+
 (add-to-list 'load-path "~/.emacs.d/custom")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/custom/emacs-color-theme-solarized")
+(add-to-list 'custom-theme-load-path
+             (file-name-as-directory "~/.emacs.d/custom/colorthemes"))
+
+
+(setq gc-cons-threshold 100000000)
 
 (defconst required-packages
   '(helm
     helm-swoop
-    helm-gtags
-    helm-cscope
-    clang-format
+    flycheck
+    yasnippet
+    elpy
     ))
 
-;(load-theme 'solarized t)
+(defun install-packages ()
+  "Install all required packages."
+  (interactive)
+  (unless package-archive-contents
+    (package-refresh-contents))
+  (dolist (package required-packages)
+    (unless (package-installed-p package)
+      (package-install package))))
 
-(global-set-key (kbd "C-x f") 'clang-format-buffer)
+(install-packages)
 
-(require 'google-c-style)
-(add-hook 'c-mode-common-hook 'google-set-c-style) ;; load google style for c-mode
-(add-hook 'c-mode-common-hook 'google-make-newline-indent) ;; automatically indent at newline
-
-;; this variables must be set before load helm-gtags
-;; you can change to any prefix key of your choice
-(setq helm-gtags-prefix-key "\C-cg")
+(load-theme 'dark-laptop t t)
+(enable-theme 'dark-laptop)
 
 (require 'setup-helm)
-(require 'setup-helm-gtags)
+(require 'yasnippet)
+(yas-global-mode 1)
 
 ;; Use space to indent by default
 (setq-default indent-tabs-mode nil)
@@ -48,9 +63,6 @@
 ;; Scroll line by line
 (setq scroll-step 1)
 
-;; Treat .h files as c++ files
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-
 ;; To use arrows to jump between windows
 (when (fboundp 'windmove-default-keybindings)
     (windmove-default-keybindings))
@@ -60,5 +72,23 @@
 (setq show-paren-delay 0)
 
 ;; Show line numbers
-;(global-linum-mode t)
-(custom-set-variables '(linum-format (quote "%4d ")))
+(global-linum-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(linum-format (quote "%4d "))
+ '(package-selected-packages
+   (quote
+    (elpy yasnippet flycheck))))
+
+(elpy-enable)
+
+(electric-indent-mode 0)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
